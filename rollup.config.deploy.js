b908be5bdadd3900b446bc3be319818d4f0cddff
Anders Christensen <89982133+McAndersC@users.noreply.github.com>
2022-05-07 00:03:05 +0200

    tested and created deploy build - full circle complete

--- rollup.config.deploy.js
+++ rollup.config.deploy.js
@@ -1,6 +1,3 @@
-const devMode = (process.env.NODE_ENV === 'development');
-console.log(`${ devMode ? 'development' : 'production' } mode bundle`);
-
 import minify from 'rollup-plugin-babel-minify';
 
 import livereload from 'rollup-plugin-livereload'
@@ -10,8 +7,7 @@ export default {
     plugins: [
         minify( {
             // Options for babel-minify.
-        } ),
-        devMode ? livereload({delay: 100}) : null
+        } )
     ],
     output: {
       file: 'build/public/index.js',
