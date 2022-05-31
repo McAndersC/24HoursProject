eea599442121957d234d6075b5b6e29aa167fd7a
Anders Christensen <89982133+McAndersC@users.noreply.github.com>
2022-05-31 09:31:21 +0200

    credits

--- src/js/credits.js
+++ src/js/credits.js
@@ -2,6 +2,8 @@ const credits = {};
 
 credits.init = async () => {
 
+    let momentsLinkable = true;
+
     let creditsPage = document.querySelector('.credits-page');
 
         if(!creditsPage) {
@@ -9,7 +11,9 @@ credits.init = async () => {
             return false;
     
         }
-    
+
+   
+
         try {
     
             const timelineData = await fetch('/public/data/students.json?' + Date.now()).then((response) => response.json()).then((response) => {
@@ -22,8 +26,9 @@ credits.init = async () => {
             const slotTemplate = (slot) => {
                 return `<section>
     
-                            <h1 class="h1 ${slot.theme}">${slot.period} <span class="h1-fat">${slot.title}</span></h1>
-    
+                            <div class="grid-credits-header">
+                                <h1 class="h1 ${slot.theme}">${slot.period} <span class="h1-fat">${slot.title}</span> - One Day Viborg</h1>
+                            </div>
                             <div class="grid-credits ${slot.id}">
 
                                 <div class="grid-credits-sizer"></div>
@@ -31,14 +36,18 @@ credits.init = async () => {
                                 ${slot.slots.map( (timeslot) => {
     
                                     return `<div class="grid-credits-item ${timeslot.format}">
-                                                <div class="grid-item-caption">
+                                                <div class="grid-item-caption ${timeslot.moments.length < 1 ? '' : 'grid-moments-pane'}">
+                                                    
                                                     <div class="grid-item-info info-time">
-                                                      ${timeslot.name}
-                                               
+                                                        ${timeslot.name}
                                                     </div>
                                                     <div class="grid-item-info info-text">
-                                                      ${timeslot.position}    
-                                                  </div>
+                                                        ${timeslot.position}
+                                                    </div>
+                                                    <div class="grid-item-info info-moments">
+                                                        ${timeslot.moments.length > 0 ? `<h4>${timeslot.name} øjeblikke.</h4>` : ''}
+                                                        ${timeslot.moments.length > 0 ? timeslot.moments.map((moment) => `<a href="/moments/${moment.id}.html">${moment.title}</a>`).join('') : ''}
+                                                    </div>
                                                 </div>   
                                     
                                                 <img src="../public/assets/portraits/${timeslot.asset}" />
@@ -61,12 +70,6 @@ credits.init = async () => {
         
             timelineData.forEach((slot) => {
         
-                // const prepageMasonry = new Masonry( '.' + slot.id, {
-                //     itemSelector: '.grid-credits-item',
-                //     columnWidth: '.grid-credits-sizer',
-                //     percentPosition: true
-                // }).layout();
-
                 imagesLoaded( '.' + slot.id, function() {
                     // init Isotope after all images have loaded
                     const prepageMasonry = new Masonry( '.' + slot.id, {
@@ -78,10 +81,20 @@ credits.init = async () => {
                 
             })
 
-            window.addEventListener("scroll", function() {
-        
+            let momentsLists = document.querySelectorAll('.grid-moments-pane');
+            // console.log('moment hvvaaa', momentsLists);
+
+            if(momentsLinkable) {
+                momentsLists.forEach( (moment) => {
     
-            })
+                    moment.addEventListener('click', (e) => {
+                        console.log('Click', e.currentTarget.classList);
+                        e.currentTarget.classList.toggle('active')
+    
+                    })
+                
+                });
+            }
     
         } catch (error) {
     
